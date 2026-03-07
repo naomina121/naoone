ecf907b29e5ac99dfeb6231d50a23a73fca3576d
naoma121 <naomina@me.com>
2026-03-07 13:15:00 +0900

    update

--- src/data/update.ts
+++ src/data/update.ts
@@ -1,18 +1,17 @@
 export const siteUpdates = [
   {
-    date: '2025-12-09',
-          updatedAt:'2025-12-14',
+    date: '2026-03-07',
     slug: 'vps-ghost-article-published',
-    title: '新記事「保護入院中の状況の中、VPS立ててGhostと戦った患者の記録」を公開しました',
-    content: '精神科の保護入院中にVPSを立ててGhostを運用した体験記を公開しました。メタ認知の観点から技術力を体験談として語っています。',
-    link:'https://naomina121.com/info-literacy/vps-ghost/',
+    title: '新記事「35年生きて、交代人格の存在に気が付いたお話」を公開しました',
+    content: '35年目にして解離性同一性障害として交代人格さんがいる事実に気が付きましたのでそのお話を語ってみようと思います。',
+    link:'https://naomina121.com/nao/did-ep1',
     category: 'Article'
   },
     {
     date: '2026-03-07',
     slug: 'one-did-ep1',
     title: '新記事「交代人格は自分の人生を生きれるのか？」を公開しました',
-    content: '交代人格が自分の人生を生きれるのかについてをone視点で語ってみました。',
+    content: '交代人格が自分の人生を生きれるのかについてを語りました。',
     link:'https://naomina121.com/one/did-ep1',
     category: 'Article'
     },
