3b59ae5f5b5c86fa1239d18bb4aaee2592b57839
naoma121 <naomina@me.com>
2026-07-31 03:38:16 +0900

    date

--- src/data/update.ts
+++ src/data/update.ts
@@ -8,136 +8,136 @@ export const siteUpdates = [
     link:'https://naomina121.com/privacy',
     category: 'Policy'
   },
-    {
-    date: '2026-05-12',
-    slug: 'nao-info-literacy-ep1',
-    title: '新記事「「社会的弱者生存論-なぜ生存権が必要なのか-」を公開しました',
-    content: '以前公開していた社会的弱者生存論の記事を再公開しました。',
-    link:'https://naomina121.com/nao/info-literacy-ep1',
-    category: 'Article'
-  },
-    {
-    date: '2026-05-12',
-    slug: 'one-did-ep8',
-    title: '新記事「「交代人格が人生の主体になりたい、あるいは結婚をしたいと思うのは罪なのか」を公開しました',
-    content: '交代人格は結婚できるのか！？基本人格の結婚を許せるのかについてを語りました。',
-    link:'https://naomina121.com/one/did-ep8',
-    category: 'Article'
-  },
-    {
-    date: '2026-05-12',
-    slug: 'one-did-ep7',
-    title: '新記事「交代人格が記憶を遠ざけるとはどういうことなのか」を公開しました',
-    content: '交代人格が記憶を遠ざけるということはどういうことなのかについてを説明しました。',
-    link:'https://naomina121.com/one/did-ep7',
-    category: 'Article'
-  },
-    {
-    date: '2026-05-12',
-    slug: 'one-did-ep6',
-    title: '新記事「交代人格が基本人格の思考・認知的フラッシュバックを守るために行っていること」を公開しました',
-    content: '思考・認知的フラッシュバックについて交代人格が語りました。',
-    link:'https://naomina121.com/one/did-ep6',
-    category: 'Article'
-  },
-    {
-    date: '2026-03-23',
-    slug: 'nao-think-ep1',
-    title: '新記事「「人は一人で生きていけない」という言葉の本当の意味を知った35歳の私」を公開しました',
-    content: 'ナオ（私）の考えが変わったのでブログ記事を更新しました。',
-    link:'https://naomina121.com/nao/think-ep1',
-    category: 'Article'
-  },
-    {
-    date: '2026-03-23',
-    slug: 'one-think-ep1',
-    title: '新記事「当事者性がある限りリアルタイムでの客観視は普通はできない」を公開しました',
-    content: '当事者性と客観性についてを語りました。',
-    link:'https://naomina121.com/one/think-ep1',
-    category: 'Article'
-    },
-    {
-    date: '2026-03-18',
-    slug: 'nao-did-ep5',
-    title: '新記事「私は、交代人格がいるか分からない時があります。」を公開しました',
-    content: '共意識についてナオ視点で語りました。',
-    link:'https://naomina121.com/nao/did-ep5',
-    category: 'Article'
-  },
-    {
-    date: '2026-03-18',
-    slug: 'one-did-ep5',
-    title: '新記事「共意識で交代人格の考えが分かる部分と分からない部分について」を公開しました',
-    content: '共意識についてone視点で語りました。',
-    link:'https://naomina121.com/one/did-ep5',
-    category: 'Article'
-    },
+  //   {
+  //   date: '2026-05-12',
+  //   slug: 'nao-info-literacy-ep1',
+  //   title: '新記事「「社会的弱者生存論-なぜ生存権が必要なのか-」を公開しました',
+  //   content: '以前公開していた社会的弱者生存論の記事を再公開しました。',
+  //   link:'https://naomina121.com/nao/info-literacy-ep1',
+  //   category: 'Article'
+  // },
+  //   {
+  //   date: '2026-05-12',
+  //   slug: 'one-did-ep8',
+  //   title: '新記事「「交代人格が人生の主体になりたい、あるいは結婚をしたいと思うのは罪なのか」を公開しました',
+  //   content: '交代人格は結婚できるのか！？基本人格の結婚を許せるのかについてを語りました。',
+  //   link:'https://naomina121.com/one/did-ep8',
+  //   category: 'Article'
+  // },
+  //   {
+  //   date: '2026-05-12',
+  //   slug: 'one-did-ep7',
+  //   title: '新記事「交代人格が記憶を遠ざけるとはどういうことなのか」を公開しました',
+  //   content: '交代人格が記憶を遠ざけるということはどういうことなのかについてを説明しました。',
+  //   link:'https://naomina121.com/one/did-ep7',
+  //   category: 'Article'
+  // },
+  //   {
+  //   date: '2026-05-12',
+  //   slug: 'one-did-ep6',
+  //   title: '新記事「交代人格が基本人格の思考・認知的フラッシュバックを守るために行っていること」を公開しました',
+  //   content: '思考・認知的フラッシュバックについて交代人格が語りました。',
+  //   link:'https://naomina121.com/one/did-ep6',
+  //   category: 'Article'
+  // },
+  //   {
+  //   date: '2026-03-23',
+  //   slug: 'nao-think-ep1',
+  //   title: '新記事「「人は一人で生きていけない」という言葉の本当の意味を知った35歳の私」を公開しました',
+  //   content: 'ナオ（私）の考えが変わったのでブログ記事を更新しました。',
+  //   link:'https://naomina121.com/nao/think-ep1',
+  //   category: 'Article'
+  // },
+  //   {
+  //   date: '2026-03-23',
+  //   slug: 'one-think-ep1',
+  //   title: '新記事「当事者性がある限りリアルタイムでの客観視は普通はできない」を公開しました',
+  //   content: '当事者性と客観性についてを語りました。',
+  //   link:'https://naomina121.com/one/think-ep1',
+  //   category: 'Article'
+  //   },
+  //   {
+  //   date: '2026-03-18',
+  //   slug: 'nao-did-ep5',
+  //   title: '新記事「私は、交代人格がいるか分からない時があります。」を公開しました',
+  //   content: '共意識についてナオ視点で語りました。',
+  //   link:'https://naomina121.com/nao/did-ep5',
+  //   category: 'Article'
+  // },
+  //   {
+  //   date: '2026-03-18',
+  //   slug: 'one-did-ep5',
+  //   title: '新記事「共意識で交代人格の考えが分かる部分と分からない部分について」を公開しました',
+  //   content: '共意識についてone視点で語りました。',
+  //   link:'https://naomina121.com/one/did-ep5',
+  //   category: 'Article'
+  //   },
 
-    {
-    date: '2026-03-15',
-    slug: 'nao-did-ep4',
-    title: '新記事「交代人格のoneについて」を公開しました',
-    content: '交代人格のoneについてを語ってみました',
-    link:'https://naomina121.com/nao/did-ep4',
-    category: 'Article'
-  },
-    {
-    date: '2026-03-15',
-    slug: 'one-did-ep4',
-    title: '新記事「基本人格のナオについて」を公開しました',
-    content: '基本人格のナオについてを語ってみました。',
-    link:'https://naomina121.com/one/did-ep4',
-    category: 'Article'
-    },
+  //   {
+  //   date: '2026-03-15',
+  //   slug: 'nao-did-ep4',
+  //   title: '新記事「交代人格のoneについて」を公開しました',
+  //   content: '交代人格のoneについてを語ってみました',
+  //   link:'https://naomina121.com/nao/did-ep4',
+  //   category: 'Article'
+  // },
+  //   {
+  //   date: '2026-03-15',
+  //   slug: 'one-did-ep4',
+  //   title: '新記事「基本人格のナオについて」を公開しました',
+  //   content: '基本人格のナオについてを語ってみました。',
+  //   link:'https://naomina121.com/one/did-ep4',
+  //   category: 'Article'
+  //   },
 
-    {
-    date: '2026-03-13',
-    slug: 'nao-did-ep3',
-    title: '新記事「離性同一性障害じゃないと証明できなかったことリスト」を公開しました',
-    content: '自分が本当にDIDなのかなって思うことを整理してみました。',
-    link:'https://naomina121.com/nao/did-ep3',
-    category: 'Article'
-  },
-    {
-    date: '2026-03-13',
-    slug: 'one-did-ep3',
-    title: '新記事「ファッションDIDの危険性」を公開しました',
-    content: 'なぜ、ファッションDIDの偽りが危険なのかを記事にしました。',
-    link:'https://naomina121.com/one/did-ep3',
-    category: 'Article'
-    },
-  {
-    date: '2026-03-08',
-    slug: 'nao-did-ep2',
-    title: '新記事「DIDになって戸惑っていること」を公開しました',
-    content: 'DIDになって私が戸惑っていることを語りました',
-    link:'https://naomina121.com/nao/did-ep2',
-    category: 'Article'
-  },
-    {
-    date: '2026-03-08',
-    slug: 'one-did-ep2',
-    title: '新記事「交代人格が増えるとどのような危険性が増えるのか」を公開しました',
-    content: '交代人買うが増える危険性を語りました。',
-    link:'https://naomina121.com/one/did-ep2',
-    category: 'Article'
-    },
-  {
-    date: '2026-03-07',
-    slug: 'nao-did-ep1',
-    title: '新記事「35年生きて、交代人格の存在に気が付いたお話」を公開しました',
-    content: '35年目にして解離性同一性障害として交代人格さんがいる事実に気が付きましたのでそのお話を語ってみようと思います。',
-    link:'https://naomina121.com/nao/did-ep1',
-    category: 'Article'
-  },
-    {
-    date: '2026-03-07',
-    slug: 'one-did-ep1',
-    title: '新記事「交代人格は自分の人生を生きれるのか？」を公開しました',
-    content: '交代人格が自分の人生を生きれるのかについてを語りました。',
-    link:'https://naomina121.com/one/did-ep1',
-    category: 'Article'
-    },
+  //   {
+  //   date: '2026-03-13',
+  //   slug: 'nao-did-ep3',
+  //   title: '新記事「離性同一性障害じゃないと証明できなかったことリスト」を公開しました',
+  //   content: '自分が本当にDIDなのかなって思うことを整理してみました。',
+  //   link:'https://naomina121.com/nao/did-ep3',
+  //   category: 'Article'
+  // },
+  //   {
+  //   date: '2026-03-13',
+  //   slug: 'one-did-ep3',
+  //   title: '新記事「ファッションDIDの危険性」を公開しました',
+  //   content: 'なぜ、ファッションDIDの偽りが危険なのかを記事にしました。',
+  //   link:'https://naomina121.com/one/did-ep3',
+  //   category: 'Article'
+  //   },
+  // {
+  //   date: '2026-03-08',
+  //   slug: 'nao-did-ep2',
+  //   title: '新記事「DIDになって戸惑っていること」を公開しました',
+  //   content: 'DIDになって私が戸惑っていることを語りました',
+  //   link:'https://naomina121.com/nao/did-ep2',
+  //   category: 'Article'
+  // },
+  //   {
+  //   date: '2026-03-08',
+  //   slug: 'one-did-ep2',
+  //   title: '新記事「交代人格が増えるとどのような危険性が増えるのか」を公開しました',
+  //   content: '交代人買うが増える危険性を語りました。',
+  //   link:'https://naomina121.com/one/did-ep2',
+  //   category: 'Article'
+  //   },
+  // {
+  //   date: '2026-03-07',
+  //   slug: 'nao-did-ep1',
+  //   title: '新記事「35年生きて、交代人格の存在に気が付いたお話」を公開しました',
+  //   content: '35年目にして解離性同一性障害として交代人格さんがいる事実に気が付きましたのでそのお話を語ってみようと思います。',
+  //   link:'https://naomina121.com/nao/did-ep1',
+  //   category: 'Article'
+  // },
+  //   {
+  //   date: '2026-03-07',
+  //   slug: 'one-did-ep1',
+  //   title: '新記事「交代人格は自分の人生を生きれるのか？」を公開しました',
+  //   content: '交代人格が自分の人生を生きれるのかについてを語りました。',
+  //   link:'https://naomina121.com/one/did-ep1',
+  //   category: 'Article'
+  //   },
 
 //     {
 //     date: '2025--07',
